4efa4b83623a6b74cc661fdc0d930e4895afdaa9
Alex Richey <alexrichey1@gmail.com>
2020-02-17 10:21:46 -0500

    Add files via upload
    Initial commit before computer wipe

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# OS
+#sudo pacman -S i3-wm
+
+# OS Small Tools
+#sudo pacman -S ripgrep
+#sudo pacman -S tree
+
+# Bigger Tools
+#sudo pacman -S clojure
+#sudo pacman -S leiningen
+
+
+# Apps
+#sudo pacman -S firefox
